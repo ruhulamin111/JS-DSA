b98da19939147b47bcb83f4772b5625d9c83bf5c
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-11-27 23:00:03 +0600

    done big o time complexity

--- app.js
+++ app.js
@@ -5,9 +5,21 @@ function sumOfNumbers(n) {
     }
     return total;
 }
-console.log(sumOfNumbers(10)) // big o n 
+// console.log(sumOfNumbers(10)) // big o n 
 
 function sumOfNumbers(n) {
     return n * (n + 1) / 2
 }
-console.log(sumOfNumbers(10)) // big o 1
+// console.log(sumOfNumbers(10)) // big o 1
+
+function multipleOfNumbers(n) {
+    for (let i = 0; i <= n; i++) {
+        for (let j = 0; j <= n; j++) {
+            console.log(i, j)
+        }
+    }
+}
+
+// console.log(multipleOfNumbers(10))// big o n2
+
+
